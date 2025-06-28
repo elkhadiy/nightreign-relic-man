f1df1082ff6b8ce828950d388cdf26b3de45c590
Yassine El Khadiri <yassineelkhadiri@gmail.com>
2025-06-28 15:00:53 +0200

    add: relic scarping script
    chore: repo cleanup

---
+++ gather_relics_from_game.py
@@ -0,0 +1,53 @@
+import pyautogui
+import pydirectinput
+import time
+import uuid
+
+
+def goto_relic_rite():
+    GOTO_RELIC_RITE_SEQUENCE = ["m", "down", "down", "down", "f"]
+    pydirectinput.write(GOTO_RELIC_RITE_SEQUENCE, interval=0.2)
+    # Move the mouse out of the way of menuing
+    pyautogui.moveTo(0, 0)
+    time.sleep(1)
+
+
+def order_relics_by_color():
+    ORDER_BY_COLOR_SEQUENCE = ["4", "f2", "f", "down", "down", "f", "q"]
+    pydirectinput.write(ORDER_BY_COLOR_SEQUENCE, interval=0.2)
+    time.sleep(1)
+
+
+def goto_first_relic():
+    pydirectinput.press("d", interval=0.2)
+    time.sleep(1)
+
+
+def grab_relic(filename = f"{uuid.uuid4()}.png"):
+    screen_size = pyautogui.size()
+
+    left_rel = 1410 / 2560
+    top_rel = 1024 / 1440
+    width_rel = 846 / 2560
+    height_rel = 306 / 1440
+
+    im = pyautogui.screenshot(region=(
+        int(screen_size.width * left_rel), int(screen_size.height * top_rel),
+        int(screen_size.width * width_rel), int(screen_size.height * height_rel)
+    ))
+
+    im.save(filename)
+
+
+if __name__ == "__main__":
+    
+    print("You have 3 seconds to focus the game!")
+    time.sleep(2)
+
+    goto_relic_rite()
+    order_relics_by_color()
+    goto_first_relic()
+
+    for i in range(88):
+        grab_relic(f"relics/{i}.png")
+        pydirectinput.press("right")
